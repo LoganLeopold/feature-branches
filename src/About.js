b959e6e0bb978e2e9074c337414a50f5776b4fa3
Amare AbebeE <amaremark@gmail.com>
2019-02-15 15:59:26 -0500

    init commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Home from "./Home"
+export default class componentName extends Component {
+  render() {
+    return (
+        ReactDOM.render(
+        
+            <Router>
+              <Home />
+            </Router>,
+    
+    )
+  }
+}
